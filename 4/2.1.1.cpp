d0a615920bbc63a0c9c7cd61980990f2df377cd3
HuKuTo3 <90009068+HuKuTo3@users.noreply.github.com>
2021-09-12 11:28:13 +0300

    Update 2.1.1.cpp

--- 4/2.1.1.cpp
+++ 4/2.1.1.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+#include <cmath>
+#include <set>
 
+using namespace std;
+
+int main()
+{
+    int x;
+    set<int> cifry;
+    cin >> x;
+    int kolvo = log10(x);
+    int array[kolvo + 1];
+    for (int i = 0; i < kolvo; i++)
+    {
+        array[i] = x % 10;
+        cifry.insert(x % 10);
+        x /= 10;
+    }
+    array[kolvo] = x;
+    cifry.insert(x);
+    if (cifry.size() == (kolvo + 1))
+    {
+        cout << "Povtora net";
+    } else
+    {
+        cout << "Povtor est'";
+    }
+}
