9d629222ac7993c589b961493f8f5ecd31b2a628
HuKuTo3 <90009068+HuKuTo3@users.noreply.github.com>
2021-09-21 01:14:28 +0300

    Update 2.1.2.cpp

--- 4/2.1.2.cpp
+++ 4/2.1.2.cpp
@@ -1,35 +1,27 @@
-#include <tchar.h>
 #include <iostream>
-#include <iomanip> 
-#include <sstream>
 #include <cmath>
-#include <vector>
  
 using namespace std;
 
-int _tmain(int argc, _TCHAR* argv[])
+int main()
 {
-    double number, number1; 
-    int k, n, sum = 0;   
-    vector<int> numbers;
+    double number, drob; 
+    int k, n, sum = 0, x = 0;   
     cin >> number >> k >> n;
-    number1 = number - floor(number);
-    stringstream ss;
-    ss << setprecision(15) <<  number;
-    string strNum = ss.str();
-    size_t pos = strNum.find('.');       
-    int NoD = strNum.size() - 1 - pos;
-    for (int i = 0; i < NoD; i++)
+    drob = number - floor(number);
+    for (int i = 0; i < k; i++)
     {
-        number1 *= 10;
-        numbers.push_back(trunc(number1));
-        number1 -= trunc(number1);
+        drob *= 10;
+        x = drob;
+        drob -= floor(drob);
     }
     for (int i = 0; i < n; i++)
     {
-        sum += numbers[k + i];
-    }
-    if (numbers[k - 1] == sum)
+        drob *= 10;
+        sum += drob;
+        drob -= floor(drob);                                                                                                                                                                                                                                                                 
+    }                                                                                                                                                                                                                                                                               int z = log10(int(x)) + 1;  if ((z + k) <= 5) sum += 1;                        
+    if (x == sum)
     {
         cout << "Ravno";
     } else 
