f6374c900c10a354cc64edbe89d721554c888d23
HuKuTo3 <90009068+HuKuTo3@users.noreply.github.com>
2021-09-21 01:10:25 +0300

    Update 2.2.cpp

--- 4/2.2.cpp
+++ 4/2.2.cpp
@@ -1,59 +1,44 @@
 #include <iostream>
+#include <iomanip>
 #include <cmath>
 
 using namespace std;
 
-long long Factorial(int x) {
-    long long sum = 1;
-    if (x > 0)
-    {
-        for (int i = 1; i <= x; i++)
-        {
-            sum *= i;
-        }
-    }
-    return sum;
-}
-
 int main()
 {
-    int x0 = -2, xn = 2;
-    double dx = 0.2, xi = x0, y;
-    for (int i = 1; xi <= xn; i++)
-    {
-        cout << xi << " ";
-        y = exp(-xi) - 1;
-        cout << y << endl;
-        xi = (x0 + i * dx);
-    }
+    double y;
+    for (float x = -2; x < (2 + 0.1); x += 0.2f) {
 
-    cout << "\n\n----------------------------------------------\n\n\n";
-
-    for (float x = -2; x <= 2.2; x += 0.2f) {
-
-        int FactorialCounter = 1;
+        long long Delit = 1;
+        double Delim = 1;
+        int Counter = 1;
         double Result = 0;
         short Sight = -1;
+        double const p = 1e-6;
 
         double OperandTemp;
+
+        y = exp(-x) - 1;
 
         if (x < -0.1 || x > 0.1)
         {
             do {
 
             double Operand;
-            Operand = Sight * (double(pow(x, FactorialCounter) / Factorial(FactorialCounter)));
-            Sight *= -1;
-            FactorialCounter++;
+            Delim *= x;
+            Delit *= Counter;
+            Operand = Sight * (double(Delim / Delit));
+            Sight = -Sight;
+            Counter++;
             Result += Operand;
             OperandTemp = Operand;
 
-            } while (abs(OperandTemp) > pow(10, -6));
-        } else
+            } while (abs(OperandTemp) > p);
+        }else
         {
             x = 0;
-            Result = 0;
+            y = 0;
         }
-        cout << x << " " << Result << endl;
+        cout  << "x = " << setw(9) <<  x << setw(12) << y << setw(12) << Result << endl;
     }
 }
