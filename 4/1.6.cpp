aaf895b20d0b3ccdd962cddbd9e86d33eea18617
HuKuTo3 <90009068+HuKuTo3@users.noreply.github.com>
2021-09-21 01:09:45 +0300

    Update 1.6.cpp

--- 4/1.6.cpp
+++ 4/1.6.cpp
@@ -1,25 +1,68 @@
 #include <iostream>
-#include <bitset>
- 
+#include <cmath>
+
 using namespace std;
- 
+
 int main() {
- 
-    short int n;
-    cin >> n;
- 
-    auto ArrayOfBits = bitset<16>(n);
- 
-    for (int i = 0; i < 16; i++)
-        cout << ArrayOfBits[i] << " ";
- 
+
+	unsigned short x, z;
+	signed char x1, x2, x3;
+
+	cout << "Enter value: ";
+	cin >> x;
+	
+    x1 = (x & 1 == 1);
+    x2 = (x & 8 == 8);
+    x3 = (x & 8192 == 8192);
+
+	int i = 15;
+	int j = 0;
+	int value = 0;
+
     cout << endl;
- 
-    cout << ArrayOfBits[0] << " " << ArrayOfBits[3] << " " << ArrayOfBits[13] << endl;  
- 
-    cout << ArrayOfBits[1].flip() << " " << ArrayOfBits[7].flip() << endl;
- 
-    for (int i = 0; i < 16; i++)
-        cout << ArrayOfBits[i] << " ";
- 
+	cout << "V dvoichnoy: " << endl;
+	while (j < 16)
+	{
+		value = pow(2, i);
+		if (x & value)
+		{
+			cout << "1 ";
+		}
+		else
+			cout << "0 ";
+		j++;
+		i--;
+	}
+    j = 0;
+	i = 15;
+
+	cout << endl;
+
+	z = x ^ 130;
+
+	cout << endl;
+	cout << "Bity: " << endl;
+	cout << (int)(x1) << " " << (int)(x2) << " " << (int)(x3) << endl;
+	cout << endl;
+
+	cout << "Do inv.: " << endl;
+	cout << x << endl << endl;
+	cout << "Posle inv.: " << endl;
+	cout << z << endl;
+	cout << endl;
+	cout << "V dvoichnoy: " << endl;
+	while (j < 16)
+	{
+		value = pow(2, i);
+		if (z & value)
+		{
+			cout << "1 ";
+		}
+		else
+			cout << "0 ";
+		j++;
+		i--;
+	}
+	cout << endl;
+	return 0;
 }
