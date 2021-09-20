df18b18577e3572309ada61ef2cade316621cf32
HuKuTo3 <90009068+HuKuTo3@users.noreply.github.com>
2021-09-21 01:11:43 +0300

    Create 2.5.cpp

---
+++ 4/2.5.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int k, i = 0, y = 0;
+    string ch;
+    cin >> k;
+    i = k;
+    do
+    {
+        int x = pow(3 , y);
+        ch = to_string(x);
+        i -= ch.size();
+        cout << ch << " " << i << endl;
+        y++;
+    } while (i >= 0);
+    cout << ch[ch.size() + i];
+}
