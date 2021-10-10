28228e95563874208cb9ec9fd4b29a90348cca1b
HuKuTo3 <90009068+HuKuTo3@users.noreply.github.com>
2021-10-10 21:23:49 +0300

    Update 5.1.cpp

--- 4/5.1.cpp
+++ 4/5.1.cpp
@@ -3,31 +3,94 @@
 
 using namespace std;
 
-int main() {
-		double x;
+double Function1(double x, int &Counter) {
 
+    Counter = 1;
+    long long Delit = 1;
+    double Delim = x / 2;
+    double Result = 0;
+    short Sight = -1;
+    double const p = 1e-6;
+    double OperandTemp;
+
+    do {
+
+        double Operand;
+        Delim *= (x / 2) * (x / 2);
+        Delit *= Counter * (Counter + 1);
+        Operand = Sight * (double(Delim / Delit));
+        Sight = -Sight;
+        Counter++;
+        Result += Operand;
+        OperandTemp = Operand;
+
+    } while (abs(OperandTemp) > p);
+
+    return Result;
+
+}
+
+void Function2(double x, double *Result2) {
+
+    int Counter = 1;
+    long long Delit = 1;
+    double Delim = x / 2;
+    short Sight = -1;
+    double const p = 1e-6;
+    double OperandTemp;
+
+    do {
+
+        double Operand;
+        Delim *= (x / 2) * (x / 2);
+        Delit *= Counter * (Counter + 1);
+        Operand = Sight * (double(Delim / Delit));
+        Sight = -Sight;
+        Counter++;
+        *Result2 += Operand;
+        OperandTemp = Operand;
+
+    } while (abs(OperandTemp) > p);
+
+}
+
+void Function3(double x, double &Result3) {
+
+    int Counter = 1;
+    long long Delit = 1;
+    double Delim = x / 2;
+    short Sight = -1;
+    double const p = 1e-6;
+    double OperandTemp;
+
+    do {
+
+        double Operand;
+        Delim *= (x / 2) * (x / 2);
+        Delit *= Counter * (Counter + 1);
+        Operand = Sight * (double(Delim / Delit));
+        Sight = -Sight;
+        Counter++;
+        Result3 += Operand;
+        OperandTemp = Operand;
+
+    } while (abs(OperandTemp) > p);
+
+}
+
+int main() {
+		double x, Result2 = 0, Result3 = 0;
 		cin >> x;
 
-		int Counter = 1;
-        long long Delit = 1;
-        double Delim = x / 2;
-        double Result = 0;
-        short Sight = -1;
-        double const p = 1e-6;
-        double OperandTemp;
+        int Counter;
+		double Result1 = Function1(x, Counter);
+        Function2(x, &Result2);
+        Function3(x, Result3);
 
-            do {
-
-            double Operand;
-            Delim *= (x / 2) * (x / 2);
-            Delit *= Counter * (Counter + 1);
-            Operand = Sight * (double(Delim / Delit));
-            Sight = -Sight;
-            Counter++;
-            Result += Operand;
-            OperandTemp = Operand;
-
-            } while (abs(OperandTemp) > p);
-
-        cout  << "x = " << setw(1) << x << setw(12) << Result << setw(3) << Counter << endl;
+        cout  << "x = " 
+        << setw(1) << x << "\n"
+        << setw(11) << Result1 << "\n"
+        << setw(11) << Result2 << "\n"
+        << setw(11) << Result3 << "\n"
+        << Counter << endl;
 }
