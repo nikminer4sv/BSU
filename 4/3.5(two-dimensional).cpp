3965f650fa2a65aa3e178d65cb93d0bc544a3d4f
HuKuTo3 <90009068+HuKuTo3@users.noreply.github.com>
2021-10-14 08:58:06 +0300

    Update and rename 3.5.cpp to 3.5(two-dimensional).cpp

--- 4/3.5(two-dimensional).cpp
+++ 4/3.5(two-dimensional).cpp
@@ -1,26 +1,30 @@
 #include <iostream>
 #include <vector>
-#include <stdlib.h>
-#include <time.h>
+#include <algorithm>
 
 using namespace std;
 
 int main(){
 
-    srand(time(NULL));
-
-    int RowsAndCols;
+    int RowsAndCols, k = 0;
 	cin >> RowsAndCols;
 
     vector<vector<int>> Matrix(RowsAndCols, vector<int>(RowsAndCols));
     
     for (int i = 0; i < RowsAndCols; i++)
-        for (int j = 0; j < RowsAndCols - i; j++)
-            Matrix[i][j] = rand() % 2 + 1;
+        for (int j = 0; j < RowsAndCols - i; j++) {
+
+            Matrix[i][j] = k + 1;
+            k = Matrix[i][j];
+
+        }
 
     for (int i = 0; i < RowsAndCols; i++) {
+
         for (int j = 0; j < RowsAndCols; j++) {
+
             cout << Matrix[i][j] << " ";
+            
         }
 
         cout << endl;
@@ -36,9 +40,16 @@ int main(){
 			for (int k = 0; k < RowsAndCols; k++)
             	AnotherMatrix[i][j] += Matrix[k][j] * Matrix[i][k];
 
+    for (int i = 0; i < RowsAndCols; i++)
+        for (int j = i; j < RowsAndCols; j++)
+            swap(AnotherMatrix[i][j],AnotherMatrix[j][i]);
+
 	for (int i = 0; i < RowsAndCols; i++) {
+
         for (int j = 0; j < RowsAndCols; j++) {
+
             cout << AnotherMatrix[i][j] << " ";
+
         }
 
         cout << endl;
