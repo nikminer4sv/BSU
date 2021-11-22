fed57539db2eda6cedaf22cf5cbcbb55d8200bdd
nikminer4sv <58385485+nikminer4sv@users.noreply.github.com>
2021-11-22 12:33:37 +0300

    UPDATA!!!!!

--- 4/6.2.cpp
+++ 4/6.2.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <cmath>
-#include <bitset>
+#include <algorithm>
 
 using namespace std;
 
@@ -10,35 +10,7 @@ private:
 
 public:
 
-    Data() {}
-
-    Data(int Day, int Month, int Year) {
-
-        
-        if (Day < 1 || Day > 31 || 
-            Month < 1 || Month > 12 || 
-            Year < 0 || Year > 99) {
-
-            cout << "Invalid data";
-            exit(1);
-
-        }
-
-
-        int* RawDay;
-        int* RawMonth;
-        int* RawYear;
-
-        RawDay = ConvertToRaw(Day,5);
-        RawMonth = ConvertToRaw(Month,4);
-        RawYear = ConvertToRaw(Year, 7);
-
-        int *RawDataArray = MergeArrays(RawDay, 5, RawMonth, 4, RawYear, 7);
-
-        ConvertToNumber(RawData, RawDataArray);
-
-    }
-
+private:
     int* ConvertToRaw(int Number, int Size) {
 
         int *Result = new int[Size];
@@ -46,11 +18,10 @@ public:
 
         while (Number > 0) {
 
-            if (Number % 2 == 0) {
+            if (Number % 2 == 0)
                 Result[Index] = 0;
-            } else {
+            else
                 Result[Index] = 1;
-            }
 
             Index += 1;
             Number  = int(Number / 2);
@@ -111,6 +82,61 @@ public:
 
     }
 
+    friend ostream& operator<< (ostream &out, Data &data) {
+    
+        if (data.GetDay() < 10)
+            out << "0" << data.GetDay();
+        else 
+            out << data.GetDay();
+
+        out << ".";
+
+        if  (data.GetMonth() < 10)
+            out << "0" << data.GetMonth();
+        else 
+            out << data.GetMonth();
+ 
+        out << ".";
+
+        if  (data.GetYear() < 10)
+            out << "200" << data.GetYear();
+        else 
+            out << "20" << data.GetYear();
+
+        return out;
+
+    }
+
+public:
+
+    Data() {}
+
+    Data(int Day, int Month, int Year) {
+
+        
+        if (Day < 1 || Day > 31 || 
+            Month < 1 || Month > 12 || 
+            Year < 0 || Year > 99) {
+
+            cout << "Invalid data";
+            exit(1);
+
+        }
+
+        int* RawDay;
+        int* RawMonth;
+        int* RawYear;
+
+        RawDay = ConvertToRaw(Day,5);
+        RawMonth = ConvertToRaw(Month,4);
+        RawYear = ConvertToRaw(Year, 7);
+
+        int *RawDataArray = MergeArrays(RawDay, 5, RawMonth, 4, RawYear, 7);
+
+        ConvertToNumber(RawData, RawDataArray);
+
+    }
+
     int GetDay() {
 
         int* RawDayArray = new int[5];
@@ -147,52 +173,122 @@ public:
 
     }
 
-    friend ostream& operator<< (ostream &out, Data &data) {
-    
-        if (data.GetDay() < 10)
-            out << "0" << data.GetDay();
-        else 
-            out << data.GetDay();
-
-        out << ".";
-
-        if  (data.GetMonth() < 10)
-            out << "0" << data.GetMonth();
-        else 
-            out << data.GetMonth();
- 
-        out << ".";
-
-        if  (data.GetYear() < 10)
-            out << "200" << data.GetYear();
-        else 
-            out << "20" << data.GetYear();
-
-        return out;
-
-    }
+    short int GetRawData() { return RawData; }
 
 
 
 };
 
+bool DataComporator(Data &a, Data &b){
+
+    if (a.GetYear() == b.GetYear()) {
+        if (a.GetMonth() == b.GetMonth()) {
+            if (a.GetDay() == b.GetDay()) {
+                return true;
+            } else {
+                return (a.GetDay() < b.GetDay()); 
+            }
+        } else {
+            return (a.GetMonth() < b.GetMonth()); 
+        }
+    } else {
+        return (a.GetYear() < b.GetYear()); 
+    }
+
+}
+
 int GetRandomInt(int min, int max) {
     return min + (rand() % static_cast<int>(max - min + 1));
+}
+
+bool SortingComporator(Data& a, Data& b) {
+
+    if (a.GetYear() == b.GetYear()) {
+
+        if (a.GetMonth() == b.GetMonth()) {
+
+            if (a.GetDay() == b.GetDay()) {
+
+                return true;
+
+            } else {
+
+                return (a.GetDay() < b.GetDay()); 
+
+            }
+
+        } else {
+
+            return (a.GetMonth() < b.GetMonth()); 
+
+        }
+
+    } else {
+
+        return (a.GetYear() < b.GetYear()); 
+
+    }
+
+}
+
+void CustomSort(Data *Array, int Size) {
+
+    for (int i = 0; i < Size; i++) {
+
+        for (int j = 0; j < Size; j++) {
+
+            if (SortingComporator(Array[i], Array[j]))
+                swap(Array[i], Array[j]);
+
+        }
+
+    }
+
 }
 
 int main() {
 
     int n;
     cin >> n;
-
     Data *Array = new Data[n];
 
+    // RANDOM INITIALIZATION START
     for (int i = 0; i < n; i++) {
 
         Array[i] = Data(GetRandomInt(1,31), GetRandomInt(1,12), GetRandomInt(0,99));
         cout << endl;
         cout << Array[i];
+        cout << Array[i].GetRawData();
 
     }
+    // RANDOM INITIALIZATON END
+
+    // REPEATING ELEMENTS START
+    cout << endl << endl << "Repeating Elements" << endl;
+
+    for (int i = 0; i < n; i++) {
+
+        int Temp = 0;
+
+        for (int j = i + 1; j < n; j++) {
+
+            if (Array[i].GetRawData() == Array[j].GetRawData())
+                cout << Array[i] << endl;
+
+        }
+
+    }
+    // REPEATING ELEMENTS END
+
+    // SORTING START
+    //sort(Array, Array + n, DataComporator);
+    CustomSort(Array, n);
+
+    cout << endl << "Sorted Elements" << endl;
+
+    for (int i = 0; i < n; i++) { 
+        cout << Array[i] << endl;
+    }
+    // SORTING END
 
 }
